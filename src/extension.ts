a31ac1096487cfde9087ba6b6e3e7cfe8da72267
Martin Norbäck Olivers <martin@norpan.org>
2018-09-16 18:47:44 +0200

    Use information from elm.json to know which files belong to which project

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
-import { workspace, ExtensionContext } from 'vscode';
+import { workspace, ExtensionContext, Uri } from 'vscode';
 
 import {
     LanguageClient,
@@ -14,11 +14,15 @@ import {
 } from 'vscode-languageclient';
 
 import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
 
 let client: LanguageClient;
 
 export async function activate(context: ExtensionContext) {
-    // We get activated if there is one or more elm.json file in the workspace
+    // We get activated if there is one or more elm.json file in the workspace.
+    // The reason we don't activate on actual elm files is that we don't know which
+    // elm.json project they belong to, since it's possible to specify crazy source-directories.
+
     // Start one server for each directory with an elm.json
     // and watch Elm files in those directories.
     let elmJsons = await workspace.findFiles("**/elm.json");
@@ -35,41 +39,55 @@ function startClient(dir: string) {
         // Client was already started for this directory
         return;
     }
+    // Read in elm.json to get information
+    let elmJson = JSON.parse(readFileSync(`${dir}/elm.json`, 'utf8'));
+    if (elmJson) {
+        // Watch elm files in source and test directories for this specific project
+        let sourceDirectories: string[] = ["tests "].concat(
+            elmJson.type === "package"
+                ? ["src", "tests"]
+                : elmJson["source-directories"]
+        );
+        let filesWatched =
+            sourceDirectories.map(d => { return { scheme: 'file', language: 'elm', pattern: `${d}/**/*` }; });
 
-    let serverPath = "elm-language-server";
+        let serverPath = "elm-language-server";
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    let serverOptions: ServerOptions = {
-        run: { command: serverPath, transport: TransportKind.stdio },
-        debug: { command: serverPath, transport: TransportKind.stdio }
-    };
+        // If the extension is launched in debug mode then the debug server options are used
+        // Otherwise the run options are used
+        let serverOptions: ServerOptions = {
+            run: { command: serverPath, transport: TransportKind.stdio },
+            debug: { command: serverPath, transport: TransportKind.stdio }
+        };
 
-    // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
-        // Register the server for Elm documents in the directory
-        documentSelector: [
-            {
-                scheme: 'file', pattern: join(dir, "**", "*.elm")
+        // Options to control the language client
+        let clientOptions: LanguageClientOptions = {
+            // Register the server for Elm documents in the directory
+            documentSelector: filesWatched,
+            // Notify the server about file changes to 'elm.json'
+            synchronize: {
+                fileEvents: workspace.createFileSystemWatcher(join(dir, 'elm.json'))
+            },
+            //
+            workspaceFolder: {
+                uri: Uri.file(dir),
+                name: dir,
+                index: 0
             }
-        ],
-        // Notify the server about file changes to 'elm.json'
-        synchronize: {
-            fileEvents: workspace.createFileSystemWatcher(join(dir, 'elm.json'))
-        }
-    };
+        };
 
-    // Create the language client and start the client.
-    client = new LanguageClient(
-        'languageServerExample',
-        'Language Server Example',
-        serverOptions,
-        clientOptions
-    );
+        // Create the language client and start the client.
+        client = new LanguageClient(
+            'languageServerExample',
+            'Language Server Example',
+            serverOptions,
+            clientOptions
+        );
 
-    // Start the client. This will also launch the server
-    client.start();
-    clients.set(dir, client);
+        // Start the client. This will also launch the server
+        client.start();
+        clients.set(dir, client);
+    }
 }
 
 export function deactivate(): Thenable<void> {
